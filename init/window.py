a4b13beb66fe0562dc4b43edb63c89ebbdbbea8b
Codesmith95 <gameknight3301@gmail.com>
2025-07-10 15:20:00 -0600

    Breakthrough update
    Added a box for the player to move around in

--- init/window.py
+++ init/window.py
@@ -4,24 +4,31 @@ from pygame.locals import *
 
 # Define the background colour
 # using RGB color coding.
-background_colour = (234, 212, 252)
+background_colour = (255, 255, 255)
+
+# Define the dimensions of screen object(width,height)
+screen = pygame.display.set_mode((0,0), pygame.RESIZABLE)
+display_info = pygame.display.Info()
+s_width = display_info.current_w
+s_height = display_info.current_h
 
 # Define the properties of the player
-x = 200
-y = 200
+x = s_width / 2
+y = s_height / 2
 width = 20
 height = 20
-vel = 10
+vel = 1
 
-# Define the dimensions of
-# screen object(width,height)
-screen = pygame.display.set_mode((900, 600))
+# Define the box the player is in
+stageSize = s_width / 4
+stageX = stageSize + (s_width / 8)
+stageY = ((s_height - stageSize) / 2) + ((s_height - stageSize) / 100)
 
 # Clock
 clock = pygame.time.Clock()
 
 # Set the caption of the screen
-pygame.display.set_caption('Avoid the projectiles')
+pygame.display.set_caption('JamFight')
 
 # Fill the background colour to the screen
 screen.fill(background_colour)
@@ -34,28 +41,33 @@ running = True
 
 # game loop
 while running:
-  ###EVENTS
+  ### EVENTS ###
   # for loop through the event queue
   for event in pygame.event.get():
     # Check for QUIT event
     if event.type == pygame.QUIT:
       running = False
 
-  ###Logic
+  ### LOGIC ###
   # Get the keys that are pressed
   keys = pygame.key.get_pressed()
-  if keys[pygame.K_LEFT] and x>0:
-    x -= 10
-  if keys[pygame.K_RIGHT] and x<900-width:
+  if keys[pygame.K_LEFT] and x > (stageX + 5):
+    x -= vel
+  if keys[pygame.K_RIGHT] and x < ((stageX + stageSize) - width - 5):
     x += vel
-  if keys[pygame.K_UP] and y>0:
+  if keys[pygame.K_UP] and y > (stageY + 5):
     y -= vel
-  if keys[pygame.K_DOWN] and y<600-height:
+  if keys[pygame.K_DOWN] and y < ((stageY + stageSize) - height - 5):
     y += vel
 
-  ###DRAW
-  # Draw the rectangle
+  ### DRAW ###
+  # Draw the background
   screen.fill(background_colour)
+
+  # Draw the box
+  pygame.draw.rect(screen, (0, 0, 0), (stageX, stageY, stageSize, stageSize), 6)
+
+  # Draw the player
   pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
 
   # UPDATE DISPLAY
