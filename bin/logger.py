5325626feb3fe7e84bdc1f258d628b2418ff99b2
Codesmith95 <gameknight3301@gmail.com>
2025-07-16 18:00:40 -0600

    Refactor logger

--- bin/logger.py
+++ bin/logger.py
@@ -6,11 +6,18 @@ import platform
 import psutil
 import os
 
+# parses system RAM info
+ram = str(round(psutil.virtual_memory().total / (1024.0 **3))) + " GB"
+# gets raw date
+rawDate = str(datetime.now())
+# parses date
+date = rawDate[:10]
+
 # Does the backbone of the work
 def main():
-	date = str(datetime.now())
 	logName = createLogName(date)
-	createLog(logName)
+	file = createLog(logName)
+	writeLog(date, ram, file)
 
 # Creates a new log with the system date
 def createLogName(date):
@@ -19,17 +26,15 @@ def createLogName(date):
 
 # Creates a new log file and writes all of the info to it
 def createLog(filename):
-	# parses system RAM info
-	ram = str(round(psutil.virtual_memory().total / (1024.0 **3))) + " GB"
-	# gets raw date
-	rawDate = str(datetime.now())
-	# parses date
-	date = rawDate[:10]
 	# open/create the log file
 	log = open("logs/" + filename, "w")
-
 	print("Logging...")
 
+  # return the log file
+	return log
+
+# Actually writes the log
+def writeLog(date, ram, log):
   # Writes log lines to the file
 	log.write("---- Running program ---\n")
 	log.write("------ " + date + " ------\n\n")
@@ -37,7 +42,3 @@ def createLog(filename):
 	log.write("Python Version: " + platform.python_version() + "\n")
 	log.write("Processor Architecture: " + str(platform.processor()) + "\n")
 	log.write("RAM: " + ram + "\n")
-
-
-
-main()
